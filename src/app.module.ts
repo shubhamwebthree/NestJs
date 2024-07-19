0ff1ebf4273d24ae93e390face67005671c0cf00
https://github.com/ShubhamWeb3 <shubham0712kumar@gmail.com>
2024-07-19 20:18:02 +0530

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+
 import { DatabaseModule } from './database/database.module';
+import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [DatabaseModule, TodoModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
